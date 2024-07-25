b33a7ecb7b67e664fedf6224cbfa45357bf9461e
margaux <margaux.tarayre@gmail.com>
2024-07-26 00:23:58 +0200

    Create a model directory and create pokemons state. Now the app display the number of pokémons in the pokedex

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM  from 'react-dom';
+import App from './App';
+
+ReactDOM.render( // Render the App component into the root element.
+    <App />,
+    document.getElementById('root') 
+);
