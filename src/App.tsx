3bd5c5a9087b648213d58729d7f8fc84e924bd65
margaux <margaux.tarayre@gmail.com>
2024-07-31 23:35:30 +0200

    Add route to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
 import React, {FunctionComponent, useState, useEffect} from 'react';
 import PokemonList from './pages/pokemon-list';
+import PokemonDetail from './pages/pokemon-detail';
+import { Link, Route, BrowserRouter as Router, Switch } from 'react-router-dom';
   
 const App: FunctionComponent = () => { // FunctionComponent is a type of Function that returns JSX.Element.
 
  return (
-        <PokemonList />
+ <Router>
+       <div>
+              {/* */}
+              <nav>
+                     <div className='nav-wrapper teal'>
+                            <Link to="/" className='brand-logo center'> Pokédex </Link>
+                     </div>
+              </nav>
+              {/* */}
+              <Switch>
+                     <Route exact path="/" component={PokemonList}/>
+                     <Route exact path="/pokemons" component={PokemonList}/>
+                     <Route exact path="/pokemons/:id" component={PokemonDetail}/>
+              </Switch>
+       </div>
+ </Router>
  )
 }
   
